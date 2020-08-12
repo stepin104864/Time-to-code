614c51fe468f1bef2d9958dc8f78e12d035b6150
stepin104864 <69066989+stepin104864@users.noreply.github.com>
2020-08-12 20:14:35 +0530

    Add files via upload

---
+++ primenumber.h
@@ -0,0 +1,16 @@
+/**
+* @file primenumber.h
+*
+*/
+#ifndef __PRIMENUMBER_H__
+#define __PRIMENUMBER_H__
+
+/**
+* Calculates the primenumber of integer number
+* @param[in] number for which primenumber function is checked
+* @return returns wheather number is prime or not
+*
+*/
+int primenumber(int number);
+
+#endif /* #ifndef __PRIMENUMBER_H__ */
